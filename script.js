6df5601b12d37d5983b747b621624d0f8841509a
Lucas-Mastaler <167825922+Lucas-Mastaler@users.noreply.github.com>
2024-04-22 21:04:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,32 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     cep = cep.substring(0, 5) + '-' + cep.substring(5); // Insere o traço na posição correta
 
     // Calcula a quilometragem usando Google Maps
-    var origem = 'CEP_ORIGEM'; // Substitua 'CEP_ORIGEM' pelo CEP de origem
+    var origem = '81030-230'; // Substitua 'CEP_ORIGEM' pelo CEP de origem
     calcularQuilometragem(origem, cep, function(quilometragem) {
         // Exibe os resultados
         document.getElementById('resultado').innerHTML = `
             <p>Entrega: ${entrega}</p>
             <p>CEP: ${cep}</p>
             <p>Região Rural: ${rural ? 'Sim' : 'Não'}</p>
-            <p>Quilometragem: ${quilometragem} km</p>
+            <p>Quilometragem: ${quilometragem}</p>
         `;
     });
 });
 
 function calcularQuilometragem(origem, destino, callback) {
-    var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' + origem + '&destinations=' + destino + 'AIzaSyAP4ZC1m9mvt_xg_pQZbPd8vnreoJk5XnY';
+    var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' + origem + '&destinations=' + destino + '&key=SUA_CHAVE_API';
 
     // Faz a requisição à API do Google Maps
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Extrai a quilometragem da resposta
-            var quilometragem = data.rows[0].elements[0].distance.text;
-            callback(quilometragem);
+            // Verifica se a resposta é válida e se há distância disponível
+            if (data.status === 'OK' && data.rows.length > 0 && data.rows[0].elements.length > 0) {
+                var quilometragem = data.rows[0].elements[0].distance.text;
+                callback(quilometragem);
+            } else {
+                callback('Erro ao calcular a quilometragem');
+            }
         })
         .catch(error => {
             console.error('Erro ao calcular a quilometragem:', error);
